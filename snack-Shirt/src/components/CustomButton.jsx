77648b126abe3953339a15af4116428b9338ddc0
Sachin7717 <sachindhimann71@gmail.com>
2024-09-14 11:33:24 +0530

    Custom button for home page

--- snack-Shirt/src/components/CustomButton.jsx
+++ snack-Shirt/src/components/CustomButton.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import state from '../store'
+import { useSnapshot } from 'valtio'
 
 
 const CustomButton = ({type, title, customStyles, handleClick}) => {
+    const snap = useSnapshot(state);
     const generateStyle = (type) => {
         if(type === 'filled') {
             return {
-                backgroundColor: snapshot.color,
+                backgroundColor: snap.color,
                 color: '#fff'
             }
         }   
     }
   return (
     <button className={`px-3 py-1.5 flex-1 rounded-md ${customStyles}`}
-    style={generateStyle(type)}>
+    style={generateStyle(type)}
+    onClick={handleClick}>
         {title}
       
     </button>
